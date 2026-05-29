b56f295c0ec71eaa5ab01eb59fbd5e7f98383cc8
bodoque-01 <sarco.snh@gmail.com>
2026-05-29 12:49:25 -0300

    Move omp_get_num_threads inside parallel section

--- parallel_integration.c
+++ parallel_integration.c
@@ -9,11 +9,11 @@ int main() {
     double pi = 0.0;
     step = 1.0/(double) num_steps;
     omp_set_num_threads(NUM_THREADS);
-    int actual_num_threads = omp_get_num_threads();
 
     #pragma omp parallel
     {
         int thread_id = omp_get_thread_num();
+        int actual_num_threads = omp_get_num_threads();
         double sum = 0.0;
         for (int i = thread_id * (num_steps/actual_num_threads); i < thread_id * (num_steps/actual_num_threads) + (num_steps/actual_num_threads); i++) {
             double x = (i + 0.5) * step;
