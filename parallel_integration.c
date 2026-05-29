d87f3e720a2c0826ac4cf00b820b7765588385e0
bodoque-01 <sarco.snh@gmail.com>
2026-05-29 12:48:02 -0300

    Use actual number of threads in interval splitting

--- parallel_integration.c
+++ parallel_integration.c
@@ -1,7 +1,6 @@
 #include "omp.h"
 #include <stdio.h>
 
-
 static long num_steps = 100000;
 double step;
 #define NUM_THREADS 4
@@ -10,12 +9,13 @@ int main() {
     double pi = 0.0;
     step = 1.0/(double) num_steps;
     omp_set_num_threads(NUM_THREADS);
+    int actual_num_threads = omp_get_num_threads();
 
     #pragma omp parallel
     {
         int thread_id = omp_get_thread_num();
         double sum = 0.0;
-        for (int i = thread_id * (num_steps/NUM_THREADS); i < thread_id * (num_steps/NUM_THREADS) + (num_steps/NUM_THREADS); i++) {
+        for (int i = thread_id * (num_steps/actual_num_threads); i < thread_id * (num_steps/actual_num_threads) + (num_steps/actual_num_threads); i++) {
             double x = (i + 0.5) * step;
             sum += (4.0/(1.0 + x*x)) * step;
         }
