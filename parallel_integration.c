3fca2aad6989c53d807e61e63887ae1aff302e26
bodoque-01 <sarco.snh@gmail.com>
2026-05-29 13:52:54 -0300

    Add context of exercise comment

--- parallel_integration.c
+++ parallel_integration.c
@@ -1,3 +1,29 @@
+/*
+
+This program will numerically compute the integral of
+
+                  4/(1+x*x) 
+				  
+from 0 to 1.  The value of this integral is pi -- which 
+is great since it gives us an easy way to check the answer.
+
+The program was parallelized using OpenMP
+
+(1) A line to include omp.h -- the include file that 
+contains OpenMP's function prototypes and constants.
+
+(2) A pragma that tells OpenMP to create a team of threads
+
+
+(3) A pragma to split up loop iterations among the team
+of threads.  This pragma includes 2 clauses to (1) create a 
+private variable and (2) to cause the threads to compute their
+sums locally and then combine their local sums into a 
+single global value.
+
+*/
+
+
 #include "omp.h"
 #include <stdio.h>
 
