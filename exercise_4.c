482993e2ee35a1accdfe3ee51d5ec0a9e94e27f3
bodoque-01 <sarco.snh@gmail.com>
2026-05-29 14:09:44 -0300

    Add run_time logging to exercise 4

--- exercise_4.c
+++ exercise_4.c
@@ -8,6 +8,7 @@ is great since it gives us an easy way to check the answer.
 
 History: Written by Tim Mattson, 11/99.
 */
+#include "omp.h"
 #include <stdio.h>
 
 static long num_steps = 100000000;
@@ -17,18 +18,29 @@ int main()
 {
     int i;
     double x, pi, sum = 0.0;
-
+    double start_time, run_time;
     step = 1.0 / (double) num_steps;
 
-    #pragma omp parallel for reduction(+:sum) private(x)
-    for (i = 1; i <= num_steps; i++) {
-        x = (i - 0.5) * step;
-        sum = sum + 4.0 / (1.0 + x * x);
+
+    start_time = omp_get_wtime();
+    #pragma omp parallel
+    {
+    #pragma omp single
+    printf(" num_threads = %d\n",omp_get_num_threads());
+
+    #pragma omp for reduction(+:sum) private(x)
+        for (i = 1; i <= num_steps; i++) {
+            x = (i - 0.5) * step;
+            sum = sum + 4.0 / (1.0 + x * x);
+            
+        }
     }
+    run_time = omp_get_wtime() - start_time;
 
     pi = step * sum;
 
     printf("pi is %f\n", pi);
+    printf("Time it took: %f\n", run_time);
 
     return 0;
 }
